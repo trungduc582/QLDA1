c488ac1e929c3b91442ff163c61913f1cefed316
DucNT <trungduc5820@gmail.com>
2022-03-26 19:46:10 +0700

    update 26

--- QLDA1/FrmDieuChuyenTienKho.Designer.cs
+++ QLDA1/FrmDieuChuyenTienKho.Designer.cs
@@ -225,7 +225,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label1.Location = new System.Drawing.Point(229, 42);
+            this.label1.Location = new System.Drawing.Point(275, 41);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(710, 46);
             this.label1.TabIndex = 226;
--- QLDA1/FrmLogin.cs
+++ QLDA1/FrmLogin.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QLDA1.Class;
 
 namespace QLDA1
 {
@@ -16,13 +17,25 @@ namespace QLDA1
         {
             InitializeComponent();
         }
+        DataTable login;
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            FrmMain f = new FrmMain();
-            f.StartPosition = FormStartPosition.CenterScreen;
-            f.Show();
-            this.Hide();
+            string sql;
+            sql = $"select * from userlogin where username ={txttk.Text.Trim()} and pwd ={txtpw.Text.Trim()}";
+            login = functions.GetDataToTable(sql);
+            if (login.Rows.Count == 0)
+            {
+                MessageBox.Show("sai tai khoan hoac mat khau!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            else
+            {
+                FrmMain f = new FrmMain();
+                f.StartPosition = FormStartPosition.CenterScreen;
+                f.Show();
+                this.Hide();
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
