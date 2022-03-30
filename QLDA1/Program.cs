10991cc122744448a2da1a7cea22caff1586070b
duc.nguyentrung <duc.nguyentrung@fssc.com.vn>
2022-03-30 20:01:57 +0700

    update 30-mar

--- QLDA1/Program.cs
+++ QLDA1/Program.cs
@@ -16,7 +16,7 @@ namespace QLDA1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmHachToan());
+            Application.Run(new FrmLogin());
         }
     }
 }
